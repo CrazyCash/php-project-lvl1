097ffd8e8d6e77f8977d00a751a1c9337ea6bc5e
CrazyCash <vazovsky.ang@gmail.com>
2021-07-24 19:31:24 +0300

    set up git actions

--- src/Cli.php
+++ src/Cli.php
@@ -1,10 +1,10 @@
-<?php 
+<?php
     namespace Brain\Games\Cli;
 
     use function cli\line;
     use function cli\prompt;
   
-    function greeting()
+    function greeting() : void
     {
         line('Welcome to the Brain Game!');
         $name = prompt('May I have your name?');
